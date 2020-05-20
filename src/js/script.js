2852e9723e819e647710f04c0c1c025837dde0f5
Motozono <voodoo.baby1984@gmail,com>
2020-05-20 16:02:32 +0900

    section faqにアコーディオン機能実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 (function () {
+  // ドロワーメニュー開閉処理
   document.getElementById("js-hamburgerToggle").onclick = function () {
     if (this.getAttribute("aria-expanded") == "false") {
       this.setAttribute("aria-expanded", "true");
@@ -15,6 +16,25 @@
     }
   };
 
+  // アコーディオン開閉処理
+  const faqToggles = document.querySelectorAll(".js-accordionToggle");
+  Array.prototype.forEach.call(faqToggles, function (faqToggle) {
+    faqToggle.addEventListener("click", function () {
+      const answer = faqToggle.nextElementSibling;
+      const faqToggleBtn = this.childNodes[3];
+      if (this.classList.contains("is-active")) {
+        this.classList.remove("is-active");
+        answer.classList.remove("is-open");
+        faqToggleBtn.setAttribute("aria-expanded", "false");
+      } else {
+        this.classList.add("is-active");
+        answer.classList.add("is-open");
+        faqToggleBtn.setAttribute("aria-expanded", "true");
+      }
+    });
+  });
+
+  // swiperオプション
   const swiper = new Swiper(".swiper-container", {
     loop: true,
     spaceBetween: 28,
